adce6143e83f64f74da47005470c610832bafac4
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-06-30 18:00:46 +0200

    converted file-endings to unix

--- omchreode.lisp
+++ omchreode.lisp
@@ -1,1 +1,14 @@
-  (in-package :om)(om::compile&load (merge-pathnames #p"sources/bpf.lisp" *load-pathname*))(om::compile&load (merge-pathnames #p"sources/chreode.lisp" *load-pathname*));--------------------------------------------------; RC subpackages initialization; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list);--------------------------------------------------(fill-library '(("generators" nil nil (get-value get-values                                       make-chreode make-tab make-tab-norm make-flip) nil)                ("dur-sampler" nil nil (make-dur-sampler get-durs) nil)                ("bpf-utils" nil nil (bpf-lookup) nil)                ("save-text" nil nil (save-text-simple save-text-coll) nil)))
+  
+(in-package :om)
+(om::compile&load (merge-pathnames #p"sources/bpf.lisp" *load-pathname*))
+(om::compile&load (merge-pathnames #p"sources/chreode.lisp" *load-pathname*))
+
+;--------------------------------------------------
+; RC subpackages initialization
+; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
+;--------------------------------------------------
+(fill-library '(("generators" nil nil (get-value get-values
+                                       make-chreode make-tab make-tab-norm make-flip) nil)
+                ("dur-sampler" nil nil (make-dur-sampler get-durs) nil)
+                ("bpf-utils" nil nil (bpf-lookup) nil)
+                ("save-text" nil nil (save-text-simple save-text-coll) nil)))
--- sources/bpf.lisp
+++ sources/bpf.lisp
@@ -1,1 +1,30 @@
-(in-package :om)(defun interpolate-bpf (bpf x)  (let ((pos (position x (om::x-points bpf) :test #'<)))    (unless pos (setq pos (- (length (om::x-points bpf)) 1)))    (decf pos)    (let* ((x-tail (nthcdr pos (om::x-points bpf)))           (x1 (first x-tail))           (x2 (second x-tail))           (y-tail (nthcdr pos (om::y-points bpf)))           (y1 (first y-tail))           (y2 (second y-tail)))      (om::interpol-segment x x1 x2 y1 y2))))(defmethod! bpf-lookup ((x number) bpf xmin xmax ymin ymax)  :doc "Allows to find the corresponding y value for a given x.The bpf is treated as if its ranges were as given by xmin, xmax, ymin, ymax."  (if (<= xmin x xmax)    (om-scale     (interpolate-bpf      bpf      (om-scale x                (list-min (x-points bpf))                (list-max (x-points bpf))                xmin xmax))      ymin ymax     (list-min (y-points bpf))     (list-max (y-points bpf)))    nil))
+(in-package :om)
+
+(defun interpolate-bpf (bpf x)
+  (let ((pos (position x (om::x-points bpf) :test #'<)))
+    (unless pos (setq pos (- (length (om::x-points bpf)) 1)))
+    (decf pos)
+    (let* ((x-tail (nthcdr pos (om::x-points bpf)))
+           (x1 (first x-tail))
+           (x2 (second x-tail))
+           (y-tail (nthcdr pos (om::y-points bpf)))
+           (y1 (first y-tail))
+           (y2 (second y-tail)))
+      (om::interpol-segment x x1 x2 y1 y2))))
+
+
+(defmethod! bpf-lookup ((x number) bpf xmin xmax ymin ymax)
+  :doc "Allows to find the corresponding y value for a given x.
+The bpf is treated as if its ranges were as given by xmin, xmax, ymin, ymax."
+  (if (<= xmin x xmax)
+    (om-scale
+     (interpolate-bpf
+      bpf
+      (om-scale x
+                (list-min (x-points bpf))
+                (list-max (x-points bpf))
+                xmin xmax)) 
+     ymin ymax
+     (list-min (y-points bpf))
+     (list-max (y-points bpf)))
+    nil))
