55815a4b36cad8f0cb215503baba740293885cbc
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-07-09 10:35:09 +0200

    added (require-library "ompw") which will be needed in the future

--- omchreode.lisp
+++ omchreode.lisp
@@ -1,14 +1,12 @@
-  
 (in-package :om)
-(om::compile&load (merge-pathnames #p"sources/bpf.lisp" *load-pathname*))
-(om::compile&load (merge-pathnames #p"sources/chreode.lisp" *load-pathname*))
 
-;--------------------------------------------------
-; RC subpackages initialization
-; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
-;--------------------------------------------------
+(require-library "ompw")
+
+(load (compile-file (merge-pathnames #p"sources/bpf.lisp" *load-pathname*)))
+(load (compile-file (merge-pathnames #p"sources/chreode.lisp" *load-pathname*)))
+
 (fill-library '(("generators" nil nil (get-value get-values
-                                       make-chreode make-tab make-tab-norm make-flip) nil)
-                ("dur-sampler" nil nil (make-dur-sampler get-durs) nil)
-                ("bpf-utils" nil nil (bpf-lookup) nil)
-                ("save-text" nil nil (save-text-simple save-text-coll) nil)))
+				       make-chreode make-tab make-tab-norm make-flip) nil)
+		("dur-sampler" nil nil (make-dur-sampler get-durs) nil)
+		("bpf-utils" nil nil (bpf-lookup) nil)
+		("save-text" nil nil (save-text-simple save-text-coll) nil)))
