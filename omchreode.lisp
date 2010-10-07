b1c496d9120f4fc146114108627cced9f3c4c812
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-10-07 09:21:41 +0200

    COPYING

--- omchreode.lisp
+++ omchreode.lisp
@@ -1,4 +1,22 @@
-;;; library load file for OM
+;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
+
+;;; This file is part of omchreode.
+
+;;; Copyright (c) 2010, Kilian Sprotte. All rights reserved.
+
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package :om)
 
 (require-library "ompw")
